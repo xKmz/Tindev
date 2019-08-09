55cf21fef2f425691666061c1def3a2f10bd19f6
Kmz in Home <reneagjunior@gmail.com>
2019-08-09 07:17:41 -0300

    app mobile start

---
+++ tindev/App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import { Text } from 'react-native';
+
+const App = () => {
+  return (
+    <Text>Hello World!</Text>
+  );
+};
+
+export default App;
