3640f89cecddf585824341560788777f4975f108
Kmz in Home <reneagjunior@gmail.com>
2019-08-10 10:02:22 -0300

    finish mobile app

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import { Link } from 'react-router-dom';
 import './Main.css'
 
@@ -7,9 +8,11 @@ import api from '../services/api';
 import logo from '../assets/logo.svg';
 import dislike from '../assets/dislike.svg';
 import like from '../assets/like.svg';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Routes({ match }) {
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(null)
 
     useEffect(() => {
         async function loadUsers() {
@@ -25,6 +28,16 @@ export default function Routes({ match }) {
         loadUsers();
     }, [match.params.id]);
     
+    useEffect(() => {
+        const socket = io('http://localhost:3333', {
+            query: { user: match.params.id }
+        });
+
+        socket.on('match', dev => {
+            setMatchDev(dev);
+        })
+    }, [match.params.id]);
+
     async function handleLike(id) {
         await api.post(`/devs/${id}/likes`, null, {
             headers: { user: match.params.id },
@@ -68,6 +81,18 @@ export default function Routes({ match }) {
                 </ul>
             ) : (
                 <div className="empty">Acabou :(</div>
+            ) }
+
+            { matchDev && (
+                <div className="match-container">
+                    <img src={itsamatch} alt="It's a match"/>
+
+                    <img className="avatar" src={matchDev.avatar} alt="" />
+                    <strong>{matchDev.name}</strong>
+                    <p>{matchDev.name}</p>
+                    
+                    <button type="button" onClick={() => setMatchDev(null)} >FECHAR</button>
+                </div>
             ) }
         </div>
     );
--- tindev/src/index.js
+++ tindev/src/index.js
@@ -7,6 +7,11 @@
  */
 
 import React, {Fragment} from 'react';
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket'
+]);
 
 import Routes from './routes';
 
